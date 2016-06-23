555b6e8e849ec548e7c651d92fe15c8e9cc9cbba
Brian Addicks <brian@addicks.us>
2016-06-22 22:29:18 -0400

    added New-VsPhase and initial tests

---
+++ powershell/cmdlets/New-VsPhase.Tests.ps1
@@ -0,0 +1,19 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "New-VsPhase" {
+    Context "no parameter is provided" {
+        It "Should fail if no parameters specified" {
+            { New-VsPhase $null } | Should Throw
+        }
+    }
+    Context "only AppType specified" {
+        It "Should return an object with the AppType defined, all other properties empty." {
+            (New-VsPhase "clioutput").AppType | Should Be "clioutput" 
+            (New-VsPhase "clioutput").NextPhase | Should BeNullOrEmpty
+            (New-VsPhase "clioutput").Fieldsets | Should BeNullOrEmpty
+            (New-VsPhase "clioutput").ErrorMessages | Should BeNullOrEmpty
+        }
+    }
+}
---
+++ powershell/cmdlets/New-VsPhase.ps1
@@ -0,0 +1,23 @@
+﻿function New-VsPhase {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [string]$AppType,
+        
+        [Parameter(Mandatory=$False,Position=1)]
+        [string]$NextPhase,
+        
+        [Parameter(Mandatory=$False)]
+        [array]$Fieldsets,
+        
+        [Parameter(Mandatory=$False)]
+        [array]$ErrorMessages = @()
+    )
+
+    $NewObject               = "" | Select AppType,NextPhase,Fieldsets,ErrorMessages
+    $NewObject.AppType       = $AppType
+    $NewObject.Fieldsets     = $Fieldsets
+    $NewObject.ErrorMessages = $ErrorMessages
+ 
+    return $NewObject
+}
