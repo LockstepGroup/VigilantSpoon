77fd1cfd484a23764c3f690b37892e3af26067e3
Brian Addicks <brian@addicks.us>
2016-06-23 15:41:21 -0400

    added tests and cmdlet for New-VsField

--- powershell/cmdlets/New-VsField.Tests.ps1
+++ powershell/cmdlets/New-VsField.Tests.ps1
@@ -3,7 +3,55 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
 Describe "New-VsField" {
-    It "does something useful" {
-        $true | Should Be $false
+    Context "return a valid text field" {
+        $NewField = New-VsField -Text -Name "My Field"
+        It "Type set to text" {
+            $NewField.Type | Should BeExactly "text"
+        }
+        It "Name set correctly" {
+            $NewField.Name | Should BeExactly "My Field"
+        }
+        It "Validators are empty" {
+            $NewField.Validators | Should BeNullOrEmpty
+        }
+    }
+    Context "return a valid checkbox" {
+        $NewField = New-VsField -Checkbox "My Checkbox"
+        It "Type set to checkbox" {
+            $NewField.Type | Should BeExactly "checkbox"
+        }
+    }
+    Context "return a valid select" {
+        $NewField = New-VsField -Select "My Checkbox" @("option1","option2")
+        It "Type set to select" {
+            $NewField.Type | Should BeExactly "select"
+        }
+        It "Options is correct length" {
+            $NewField.Options.Count | Should Be 2
+        }
+    }
+    Context "check optional parameters set" {
+        $NewField = New-VsField -Text -Name "My Field" -Label "My Label" -Inline -Example "Example Text"
+        It "Label set correctly" {
+            $NewField.Label | Should Be "My Label"
+        }
+        It "Inline is set to 'true'" {
+            $NewField.Inline | Should Be "true"
+        }
+        It "Example set correctly" {
+            $NewField.Example | Should Be "Example Text"
+        }
+    }
+    Context "check optional parameters set" {
+        $NewField = New-VsField -Text -Name "My Field"
+        It "Label is empty" {
+            $NewField.Label | Should BeNullOrEmpty
+        }
+        It "Inline is set to 'false'" {
+            $NewField.Inline | Should Be "false"
+        }
+        It "Example is null" {
+            $NewField.Example | Should BeNullOrEmpty
+        }
     }
 }
--- powershell/cmdlets/New-VsField.ps1
+++ powershell/cmdlets/New-VsField.ps1
@@ -1,3 +1,58 @@
 ﻿function New-VsField {
+    [CmdletBinding()]
 
+    Param (
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Name,
+        
+        [Parameter(Mandatory=$False)]
+        [string]$Label,
+        
+        [Parameter(Mandatory=$False)]
+        [switch]$Inline,
+        
+        # text
+        [Parameter(Mandatory=$True,Position=0,ParameterSetName="text")]
+        [switch]$Text,
+        
+        [Parameter(Mandatory=$False,Position=2,ParameterSetName="text")]
+        [string]$Placeholder,
+        
+        [Parameter(Mandatory=$False,ParameterSetName="text")]
+        [array]$Validators = @(),
+
+        [Parameter(Mandatory=$False,ParameterSetName="text")]
+        [string]$Example,
+        
+        # select
+        [Parameter(Mandatory=$True,Position=0,ParameterSetName="select")]
+        [switch]$Select,
+        
+        [Parameter(Mandatory=$False,Position=2,ParameterSetName="select")]
+        [array]$Options,
+        
+        # checkbox
+        [Parameter(Mandatory=$True,Position=0,ParameterSetName="checkbox")]
+        [switch]$Checkbox
+    )
+
+    if ($Text)     { $Type = "text" }
+    if ($Select)   { $Type = "select"}
+    if ($Checkbox) { $Type = "checkbox"}
+
+    $NewObject             = "" | Select Name,Type,Placeholder,Validators,Label,Options,Inline,Example
+    $NewObject.Name        = $Name
+    $NewObject.Type        = $Type
+    $NewObject.Label       = $Label
+    $NewObject.Placeholder = $Placeholder
+    $NewObject.Options     = $Options
+    $NewObject.Example     = $Example
+    
+    if ($Inline) {
+        $NewObject.Inline = "true"
+    } else {
+        $NewObject.Inline = "false"
+    }
+
+    return $NewObject
 }
