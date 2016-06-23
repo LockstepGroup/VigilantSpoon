c6ca6548887d0e9884e14c5b8e0a1e4fbf07932e
Brian Addicks <brian@addicks.us>
2016-06-23 14:58:38 -0400

    added all existing cmdlets and tests (skeleton files), completed cmdlet and tests for New-VsCliResult

---
+++ powershell/cmdlets/New-VsCliResult.Tests.ps1
@@ -0,0 +1,11 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "New-VsCliResult" {
+    It "returns a valid CliResult object" {
+        $NewCliResult = New-VsCliResult "My Header" "My Content"
+        $NewCliResult.Header  | Should be "My Header"
+        $NewCliResult.Content | Should be "My Content"
+    }
+}
---
+++ powershell/cmdlets/New-VsCliResult.ps1
@@ -0,0 +1,16 @@
+﻿function New-VsCliResult {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [string]$Header,
+        
+        [Parameter(Mandatory=$False,Position=1)]
+        [string]$Content
+    )
+    
+    $NewObject         = "" | Select Header,Content
+    $NewObject.Header  = $Header 
+    $NewObject.Content = $Content
+    
+    return $NewObject
+}
