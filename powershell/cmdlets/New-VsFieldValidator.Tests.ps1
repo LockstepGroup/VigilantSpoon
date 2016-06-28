a1c02e30bc703670dbe3886157cc4c9d17bd0b56
Brian Addicks <brian@addicks.us>
2016-06-28 09:56:55 -0400

    completed tests/cmdlet for New-VsFieldValidator

--- powershell/cmdlets/New-VsFieldValidator.Tests.ps1
+++ powershell/cmdlets/New-VsFieldValidator.Tests.ps1
@@ -4,17 +4,110 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\New-VsHtmlAttribute.ps1"
 
 Describe "New-VsFieldValidator" {
-    
-    #New-VsHtmlAttribute ("data-fv-$Type").ToLower() "true"
-    Context "Validator: different" {
-        $Type                  = "different"
-        $Params                = @{}
+    $Params                = @{}
+    $Params.ErrorMessage   = "myerror"
+    Context "Validator: Different" {
+        $Type                  = "Different"
         $Params.$Type          = $true
-        $Params.ErrorMessage   = "myerror"
         $Params.CompareToField = "myfield"
         $NewValidator          = New-VsFieldValidator @Params
         It "data-fv-$Type = `'true`'" {
             ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type" }).Value | Should BeExactly 'true'
         }
+        It "data-fv-$Type-message = `'$($Params.ErrorMessage)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-message" }).Value | Should BeExactly $Params.ErrorMessage
+        }
+        It "data-fv-$Type-field = `'$($Params.CompareToField)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-field" }).Value | Should BeExactly $Params.CompareToField
+        }
+        $Params.Remove($Type)
+        $Params.Remove("CompareToField")
+    }
+    Context "Validator: Ip" {
+        $Type         = "Ip"
+        $Params.$Type = $true
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type = `'true`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type" }).Value | Should BeExactly 'true'
+        }
+        It "data-fv-$Type-message = `'$($Params.ErrorMessage)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-message" }).Value | Should BeExactly $Params.ErrorMessage
+        }
+        It "No IP version settings set when excluded" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-ipv4" }).Value | Should BeNullOrEmpty
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-ipv6" }).Value | Should BeNullOrEmpty
+        }
+        $Params.IPv4Only = $true
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type-ipv6 is `'false`' with -IPv4Only" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-ipv6" }).Value | Should BeExactly 'false'
+        }
+        $Params.Remove('IPv4Only')
+        $Params.IPv6Only = $true
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type-ipv4 is `'false`' with -IPv6Only" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-ipv4" }).Value | Should BeExactly 'false'
+        }
+        $Params.Remove('IPv6Only')
+        $Params.Remove($Type)
+    }
+    Context "Validator: NotEmpty" {
+        $Type         = "NotEmpty"
+        $Params.$Type = $true
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type = `'true`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type" }).Value | Should BeExactly 'true'
+        }
+        It "data-fv-$Type-message = `'$($Params.ErrorMessage)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-message" }).Value | Should BeExactly $Params.ErrorMessage
+        }
+        $Params.Remove($Type)
+    }
+    Context "Validator: RegularExpression" {
+        $Type           = "RegularExpression"
+        $Params.$Type   = $true
+        $Params.Pattern = "mypattern"
+        $NewValidator   = New-VsFieldValidator @Params
+        It "data-fv-regexp = `'true`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-regexp" }).Value | Should BeExactly 'true'
+        }
+        It "data-fv-regexp-message = `'$($Params.ErrorMessage)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-regexp-message" }).Value | Should BeExactly $Params.ErrorMessage
+        }
+        It "data-fv-regexp-regexp = `'$($Params.Pattern)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-regexp-regexp" }).Value | Should BeExactly $Params.Pattern
+        }
+        $Params.Remove($Type)
+        $Params.Remove("Pattern")
+    }
+    Context "Validator: Between" {
+        $Type         = "Between"
+        $Params.$Type = $true
+        $Params.Min   = 0
+        $Params.Max   = 30
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type = `'true`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type" }).Value | Should BeExactly 'true'
+        }
+        It "data-fv-$Type-message = `'$($Params.ErrorMessage)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-message" }).Value | Should BeExactly $Params.ErrorMessage
+        }
+        It "data-fv-$Type-min = `'$($Params.Min)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-min" }).Value | Should BeExactly $Params.Min
+        }
+        It "data-fv-$Type-max = `'$($Params.Max)`'" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-max" }).Value | Should BeExactly $Params.Max
+        }
+        It "data-fv-$Type-inclusive = `'false`' without -Inclusive" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-inclusive" }).Value | Should BeNullOrEmpty
+        }
+        $Params.Inclusive = $true
+        $NewValidator = New-VsFieldValidator @Params
+        It "data-fv-$Type-inclusive = `'true`' with -Inclusive" {
+            ($NewValidator | ? { $_.Attribute -eq "data-fv-$Type-inclusive" }).Value | Should BeExactly 'true'
+        }
+        $Params.Remove("Min")
+        $Params.Remove("Max")
+        $Params.Remove("Inclusive")
     }
 }
--- powershell/cmdlets/New-VsFieldValidator.ps1
+++ powershell/cmdlets/New-VsFieldValidator.ps1
@@ -10,13 +10,72 @@
         [switch]$Different,
         
         [Parameter(Mandatory=$True,ParameterSetName="different",Position=1)]
-        [string]$CompareToField
+        [string]$CompareToField,
+
+        # ip
+        [Parameter(Mandatory=$True,ParameterSetName="ip",Position=0)]
+        [switch]$Ip,
+        
+        [Parameter(Mandatory=$False,ParameterSetName="ip")]
+        [switch]$IPv4Only,
+        
+        [Parameter(Mandatory=$False,ParameterSetName="ip")]
+        [switch]$IPv6Only,
+        
+        # notempty
+        [Parameter(Mandatory=$True,ParameterSetName="notempty",Position=0)]
+        [switch]$NotEmpty,
+        
+        # regexp
+        [Parameter(Mandatory=$True,ParameterSetName="regexp",Position=0)]
+        [switch]$RegularExpression,
+        
+        [Parameter(Mandatory=$True,ParameterSetName="regexp",Position=1)]
+        [string]$Pattern,
+        
+        # between
+        [Parameter(Mandatory=$True,ParameterSetName="between",Position=0)]
+        [switch]$Between,
+        
+        [Parameter(Mandatory=$True,ParameterSetName="between",Position=1)]
+        [double]$Min,
+        
+        [Parameter(Mandatory=$True,ParameterSetName="between",Position=2)]
+        [double]$Max,
+        
+        [Parameter(Mandatory=$False,ParameterSetName="between")]
+        [switch]$Inclusive
     )
 
     if ($Different)         { $Type = "different" }
+    if ($Ip)                { $Type = "ip"        }
+    if ($NotEmpty)          { $Type = "notempty"  }
+    if ($RegularExpression) { $Type = "regexp"    }
+    if ($Between)           { $Type = "between"   }
     
     $NewObject = @()
     $NewObject += New-VsHtmlAttribute ("data-fv-$Type").ToLower() "true"
+    $NewObject += New-VsHtmlAttribute ("data-fv-$Type-message").ToLower() $ErrorMessage
+
+    switch ($Type) {
+        "different" {
+            $NewObject += New-VsHtmlAttribute "data-fv-different-field" $CompareToField
+            break
+        }
+        "ip" {
+            if ($IPv4Only) { $NewObject += New-VsHtmlAttribute "data-fv-ip-ipv6" 'false' }
+            if ($IPv6Only) { $NewObject += New-VsHtmlAttribute "data-fv-ip-ipv4" 'false' }
+            break
+        }
+        "regexp" {
+            $NewObject += New-VsHtmlAttribute "data-fv-regexp-regexp" $Pattern
+        }
+        "between" {
+            $NewObject += New-VsHtmlAttribute "data-fv-between-min" $Min
+            $NewObject += New-VsHtmlAttribute "data-fv-between-max" $Max
+            if ($Inclusive) { $NewObject += New-VsHtmlAttribute "data-fv-between-inclusive" "true" }
+        }
+    }
 
     return $NewObject    
 }
